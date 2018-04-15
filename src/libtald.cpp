a067580fb5f01daf1b019d3fd76446feda7bf546
Unknown <margoke57@gmail.com>
2018-04-15 13:47:00 +0300

    Revert "Demux and mux selecting fix"
    This reverts commit 3d76301b08ef742885903e11801300fa1eab74ff.

--- src/libtald.cpp
+++ src/libtald.cpp
@@ -16,16 +16,10 @@ tald_t gTald({{TLD_DATA_EN, {TLD_ADDR_OUT, TLD_ADDR_OMASK}, 0},
 void vTaldInit() {}
 
 int iTaldSample(sensor_matrix_t &smx) {
-  for (size_t x = 1; x < 2; x++) {   // Rida TLD_SMX_X
-    for (size_t y = 1; y < 2; y++) { // Veerg TLD_SMX_Y
-      // smx[x][y] = (uint8_t)y;                // debug
-      uint16_t sample_u16 = gTald.read(x, y);
-
-      uint8_t sample_u8 = (sample_u16 * 255.0) / 4095.0; // 1-4095
-
-      smx[x][y] = sample_u8;
-
-      // smx[x][y] = (uint8_t)gTald.read(x, y) << 8;
+  for (size_t x = 0; x < TLD_SMX_X; x++) {
+    for (size_t y = 0; y < TLD_SMX_Y; y++) {
+      smx[x][y] = (uint8_t)(gTald.read(x, y) >> 8);
+      // smx[x][y] = (uint8_t)y;
     }
   }
   return 0;
